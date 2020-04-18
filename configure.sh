6f7ca33d9514ce646b8d5b61c31337e74e3549e4
Alexandre Rangel <mamutal91@gmail.com>
2020-04-18 01:33:31 -0300

    configure: Add lang en_US

--- configure.sh
+++ configure.sh
@@ -15,6 +15,8 @@ mv files/mirrorlist /etc/pacman.d/
 sudo pacman -Sy efibootmgr git grub nano refind-efi sudo wget --needed --noconfirm
 
 echo "Set locale and zone"
+sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g" /etc/locale.gen
+sed -i "s/#en_US ISO-8859-1/en_US ISO-8859-1/g" /etc/locale.gen
 sed -i "s/#pt_BR.UTF-8 UTF-8/pt_BR.UTF-8 UTF-8/g" /etc/locale.gen
 sed -i "s/#pt_BR ISO-8859-1/pt_BR ISO-8859-1/g" /etc/locale.gen
 locale-gen
