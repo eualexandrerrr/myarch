9bf461390edbd410140f90f73c610c00f85f70ea
Alexandre Rangel <mamutal91@gmail.com>
2020-11-17 08:23:07 -0300

    adjust echo

--- 3-configure.sh
+++ 3-configure.sh
@@ -64,4 +64,4 @@ refind-install --usedefault /dev/sda1
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "exit / umount -R / reboot"
+echo "exit   /   umount -R /mnt   >   reboot"
