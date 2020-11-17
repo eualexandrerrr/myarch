776b514aaa6525687d51b7d68dc0c958dc7b3598
Alexandre Rangel <mamutal91@gmail.com>
2020-11-17 08:25:42 -0300

    zram

--- 3-configure.sh
+++ 3-configure.sh
@@ -9,7 +9,7 @@ sed -i "s/#Color/Color/g" /etc/pacman.conf
 
 modprobe zram
 echo lz4 > /sys/block/zram0/comp_algorithm
-sed -i "s/0/32GB/g" /sys/block/zram0/disksize
+echo 32G > /sys/block/zram0/disksize
 mkswap --label zram0 /dev/zram0
 swapon --priority 100 /dev/zram0
 
