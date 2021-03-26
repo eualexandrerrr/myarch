2ba941d0811be820b0c4e24469487beab1b0b864
Alexandre Rangel <mamutal91@gmail.com>
2021-03-26 20:50:34 -0300

    Reduces the swap
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- install.sh
+++ install.sh
@@ -108,7 +108,7 @@ else
   # Format the drive
   sgdisk -g --clear \
     --new=1:0:+1GiB   --typecode=1:ef00 --change-name=1:EFI \
-    --new=2:0:+16GiB  --typecode=2:8200 --change-name=2:cryptswap \
+    --new=2:0:+8GiB   --typecode=2:8200 --change-name=2:cryptswap \
     --new=3:0:0       --typecode=3:8300 --change-name=3:cryptsystem \
     $DISK
     if [[ $? -eq 0 ]]; then
