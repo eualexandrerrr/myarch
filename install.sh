ee0ced90a0773accf51daac507c525f1af3623fa
Alexandre Rangel <mamutal91@gmail.com>
2021-06-17 21:22:16 -0300

    Config pacman
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Config pacman
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+sed -i "s/#Color/Color/g" /etc/pacman.conf
+sed -i "s/#UseSyslog/UseSyslog/g" /etc/pacman.conf
+sed -i "s/#VerbosePkgLists/VerbosePkgLists/g" /etc/pacman.conf
+sed -i "s/#ParallelDownloads/ParallelDownloads/g" /etc/pacman.conf
+
 if [[ ${1} == "recovery" ]]; then
   echo "Unlock and mount /dev/sda2"
   cryptsetup luksOpen /dev/sda2 lvm
