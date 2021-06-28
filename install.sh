1b6aed8f7e87b75976500ede1bd2f9e040d38acb
Alexandre Rangel <mamutal91@gmail.com>
2021-06-27 22:01:51 -0300

    remove quotes
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sed -i "s/#UseSyslog/UseSyslog/g" /etc/pacman.conf
 sed -i "s/#VerbosePkgLists/VerbosePkgLists/g" /etc/pacman.conf
 sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 10/g" /etc/pacman.conf
 
-if [[ ${1} == "recovery" ]]; then
+if [[ ${1} == recovery ]]; then
   echo "Unlock and mount /dev/sda2"
   cryptsetup luksOpen /dev/sda2 lvm
   mount /dev/mapper/arch-root /mnt
