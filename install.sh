e1fbb13c0a88c3d74e3b542a096ccf07af9e264c
Alexandre Rangel <mamutal91@aospk.org>
2021-07-10 19:30:46 -0300

    Increasing boot partition size
    Signed-off-by: Alexandre Rangel <mamutal91@aospk.org>

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ if [[ ${1} == recovery ]]; then
 else
   # Format the drive
   sgdisk --clear \
-    --new=1:0:+550MiB --typecode=1:ef00 --change-name=1:EFI \
+    --new=1:0:+1GiB   --typecode=1:ef00 --change-name=1:EFI \
     --new=2:0:+16GiB  --typecode=2:8200 --change-name=2:cryptswap \
     --new=3:0:0       --typecode=3:8300 --change-name=3:cryptsystem \
     $DISK
