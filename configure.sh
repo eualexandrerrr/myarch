3805b7b38b273668c0da55c5078dcb05e4ae12bd
Alexandre Rangel <mamutal91@gmail.com>
2022-03-20 15:41:44 -0300

    W.I.P
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- configure.sh
+++ configure.sh
@@ -4,105 +4,106 @@
 SSD2_UUID=$(blkid $SSD2 | awk -F '"' '{print $2}')
 SSD3_UUID=$(blkid $SSD3 | awk -F '"' '{print $2}')
 
-# Set user and hostname
-useradd -m -G wheel -s /bin/bash $USERNAME
-mkdir -p /home/$USERNAME
-echo $HOSTNAME > /etc/hostname
+source colors.sh
 
-# Configure hosts
-echo "127.0.0.1	localhost
-::1		localhost
-127.0.1.1	${HOSTNAME}" | tee /etc/hosts
+createUseraAndHost() {
+  useradd -m -G wheel -s /bin/bash $USERNAME
+  mkdir -p /home/$USERNAME
+  echo $HOSTNAME > /etc/hostname
+  chown -R $USERNAME:$USERNAME /home/$USERNAME
+  echo "127.0.0.1	localhost
+  ::1		localhost
+  127.0.1.1	${HOSTNAME}" | tee /etc/hosts
+}
 
-# Discover the best mirros to download packages and update pacman configs
-reflector --verbose -c BR --protocol https --protocol http --sort rate --save /etc/pacman.d/mirrorlist
-sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-sed -i 's/#UseSyslog/UseSyslog/' /etc/pacman.conf
-sed -i 's/#Color/Color\\\nILoveCandy/' /etc/pacman.conf
-sed -i 's/Color\\/Color/' /etc/pacman.conf
-sed -i 's/#TotalDownload/TotalDownload/' /etc/pacman.conf
-sed -i 's/#CheckSpace/CheckSpace/' /etc/pacman.conf
-sed -i "s/#VerbosePkgLists/VerbosePkgLists/g" /etc/pacman.conf
-sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 20/g" /etc/pacman.conf
+reflectorMirrors() {
+  reflector --verbose -c BR --protocol https --protocol http --sort rate --save /etc/pacman.d/mirrorlist
+  sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+  sed -i 's/#UseSyslog/UseSyslog/' /etc/pacman.conf
+  sed -i 's/#Color/Color\\\nILoveCandy/' /etc/pacman.conf
+  sed -i 's/Color\\/Color/' /etc/pacman.conf
+  sed -i 's/#TotalDownload/TotalDownload/' /etc/pacman.conf
+  sed -i 's/#CheckSpace/CheckSpace/' /etc/pacman.conf
+  sed -i "s/#VerbosePkgLists/VerbosePkgLists/g" /etc/pacman.conf
+  sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 20/g" /etc/pacman.conf
+}
 
-# Setup locate and time
-echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
-sed -i "s/#pt_BR.UTF-8 UTF-8/pt_BR.UTF-8 UTF-8/g" /etc/locale.gen
-sed -i "s/#pt_BR ISO-8859-1/pt_BR ISO-8859-1/g" /etc/locale.gen
-sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g" /etc/locale.gen
-sed -i "s/#en_US ISO-8859-1/en_US ISO-8859-1/g" /etc/locale.gen
-echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
-locale-gen
-ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
-hwclock --systohc
+localeAndTime() {
+  echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
+  sed -i "s/#pt_BR.UTF-8 UTF-8/pt_BR.UTF-8 UTF-8/g" /etc/locale.gen
+  sed -i "s/#pt_BR ISO-8859-1/pt_BR ISO-8859-1/g" /etc/locale.gen
+  sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g" /etc/locale.gen
+  sed -i "s/#en_US ISO-8859-1/en_US ISO-8859-1/g" /etc/locale.gen
+  echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
+  locale-gen
+  ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+  hwclock --systohc
+}
 
-# Generate the initramfs
-sed -i "s/BINARIES=()/BINARIES=(btrfs)/g" /etc/mkinitcpio.conf
-sed -i "s/block/block encrypt/g" /etc/mkinitcpio.conf
-sed -i "s/#COMPRESSION=\"zstd\"/COMPRESSION=\"zstd\"/g" /etc/mkinitcpio.conf
-sed -i "s/#COMPRESSION_OPTIONS=()/COMPRESSION_OPTIONS=(-9)/g" /etc/mkinitcpio.conf
+mkinitcpioConfigs() {
+  sed -i "s/BINARIES=()/BINARIES=(btrfs)/g" /etc/mkinitcpio.conf
+  sed -i "s/block/block encrypt/g" /etc/mkinitcpio.conf
+  sed -i "s/#COMPRESSION=\"zstd\"/COMPRESSION=\"zstd\"/g" /etc/mkinitcpio.conf
+  sed -i "s/#COMPRESSION_OPTIONS=()/COMPRESSION_OPTIONS=(-9)/g" /etc/mkinitcpio.conf
+  mkinitcpio -P
+}
 
-mkinitcpio -P
+bootloaderConfigs() {
+  bootctl --path=/boot install
+  mkdir -p /boot/loader/entries
+  "title   Arch Linux
+  linux   /vmlinuz-linux
+  initrd  /initramfs-linux.img
+  options rd.luks.name=${SSD3_UUID}=$DISK_NAME root=/dev/mapper/$DISK_NAME rootflags=subvol=root rd.luks.options=discard rw" | tee /boot/loader/entries/arch.conf
 
-# Setup the bootloader
-bootctl --path=/boot install
+  echo "default  arch.conf
+  timeout  4
+  console-mode max
+  editor   no" | tee /boot/loader/loader.conf
+}
 
-# Generate the linux entry config
-mkdir -p /boot/loader/entries
-cat > /boot/loader/entries/arch.conf << EOF
-title   Arch Linux
-linux   /vmlinuz-linux
-initrd  /initramfs-linux.img
-options rd.luks.name=${SSD3_UUID}=$DISK_NAME root=/dev/mapper/$DISK_NAME rootflags=subvol=root rd.luks.options=discard rw
-EOF
+grubConfigs() {
+  sed -i 's/GRUB_DEFAULT=0/GRUB_DEFAULT=saved/g' /etc/default/grub
+  sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet splash acpi_backlight=vendor"/g' /etc/default/grub
+  sed -i -e 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cryptdevice=UUID='${SSD3_UUID}':cryptsystem"/g' /etc/default/grub
+  sed -i 's/#GRUB_ENABLE_CRYPTODISK=y/GRUB_ENABLE_CRYPTODISK=y/g' /etc/default/grub
+  sed -i 's/#GRUB_SAVEDEFAULT=true/GRUB_SAVEDEFAULT=true/g' /etc/default/grub
+  sed -i 's/#GRUB_DISABLE_SUBMENU=y/GRUB_DISABLE_SUBMENU=y/g' /etc/default/grub
+  grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
+  grub-mkconfig -o /boot/grub/grub.cfg
+}
 
-# Generate the loader config
-cat > /boot/loader/loader.conf << EOF
-default  arch.conf
-timeout  4
-console-mode max
-editor   no
-EOF
+systemdConfigs() {
+  sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=ignore/g' /etc/systemd/logind.conf
+  sed -i 's/#NAutoVTs=6/NAutoVTs=6/g' /etc/systemd/logind.conf
+}
 
-# Configure grub
-sed -i 's/GRUB_DEFAULT=0/GRUB_DEFAULT=saved/g' /etc/default/grub
-sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet splash acpi_backlight=vendor"/g' /etc/default/grub
-sed -i -e 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cryptdevice=UUID='${SSD3_UUID}':cryptsystem"/g' /etc/default/grub
-sed -i 's/#GRUB_ENABLE_CRYPTODISK=y/GRUB_ENABLE_CRYPTODISK=y/g' /etc/default/grub
-sed -i 's/#GRUB_SAVEDEFAULT=true/GRUB_SAVEDEFAULT=true/g' /etc/default/grub
-sed -i 's/#GRUB_DISABLE_SUBMENU=y/GRUB_DISABLE_SUBMENU=y/g' /etc/default/grub
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
-grub-mkconfig -o /boot/grub/grub.cfg
+sshConfigs() {
+  pwd=$(pwd)
+    rm -rf /etc/ssh/ssh_config
+    cd /etc/ssh
+    wget https://raw.githubusercontent.com/openssh/openssh-portable/master/ssh_config
+    chown -R root:root ssh_config
+  cd $pwd
+  sed -i "s/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g" /etc/ssh/ssh_config
+  sed -i "s/#AllowAgentForwarding yes/AllowAgentForwarding yes/g" /etc/ssh/sshd_config
+  sed -i "s/#AllowTcpForwarding yes/AllowTcpForwarding yes/g" /etc/ssh/sshd_config
+}
 
-# Configure systemd for laptop's
-sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=ignore/g' /etc/systemd/logind.conf
-sed -i 's/#NAutoVTs=6/NAutoVTs=6/g' /etc/systemd/logind.conf
+systemctlConfigs() {
+  systemctl disable NetworkManager
+  systemctl enable dhcpcd
+  systemctl enable iwd
+  systemctl enable sshd.service
+}
 
-# SSHD
-pwd=$(pwd)
-  rm -rf /etc/ssh/ssh_config
-  cd /etc/ssh
-  wget https://raw.githubusercontent.com/openssh/openssh-portable/master/ssh_config
-  chown -R root:root ssh_config
-cd $pwd
-sed -i "s/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g" /etc/ssh/ssh_config
-sed -i "s/#AllowAgentForwarding yes/AllowAgentForwarding yes/g" /etc/ssh/sshd_config
-sed -i "s/#AllowTcpForwarding yes/AllowTcpForwarding yes/g" /etc/ssh/sshd_config
+sudoersConfigs() {
+  sed -i "s/root ALL=(ALL:ALL) ALL/root ALL=(ALL:ALL) NOPASSWD: ALL\n${USERNAME} ALL=(ALL:ALL) NOPASSWD: ALL/g" /etc/sudoers
+  sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL$/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+  echo "Defaults timestamp_timeout=0" >> /etc/sudoers
+}
 
-# Services
-systemctl disable NetworkManager
-systemctl enable dhcpcd
-systemctl enable iwd
-systemctl enable sshd.service
-
-# Sudo configs
-sed -i "s/root ALL=(ALL:ALL) ALL/root ALL=(ALL:ALL) NOPASSWD: ALL\n${USERNAME} ALL=(ALL:ALL) NOPASSWD: ALL/g" /etc/sudoers
-sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL$/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
-echo "Defaults timestamp_timeout=0" >> /etc/sudoers
-
-# My notebook
-mountStorages() {
-  # Storage 2
+mountStorage() {
   STORAGE_HDD_UUID=$(blkid $STORAGE_HDD | awk -F '"' '{print $2}')
   mkdir -p /mnt/hdd
   echo -e "\nhdd UUID=$STORAGE_HDD_UUID /root/keyHDD luks" >> /etc/crypttab
@@ -111,29 +112,37 @@ mountStorages() {
   dd if=/dev/urandom of=/root/keyHDD bs=1024 count=4
   chmod 0400 /root/keyHDD
   clear
-  echo "Type crypt password $STORAGE_HDD"
+  echo -e "\n${BOL_GRE}Digite a senha destravar o ${MAG}${STORAGE_HDD}${END}"
   cryptsetup -v luksAddKey $STORAGE_HDD /root/keyHDD
 }
 
-if [[ $USERNAME == mamutal91 ]]; then
-  mountStorages
-fi
-
-# Define passwords
-clear
-echo "Type user password $USERNAME"
-passwd $USERNAME && clear
-echo "Type user password root"
-passwd root
+passwords() {
+  clear
+  echo -e "\n${BOL_GRE}Digite a senha para ${MAG}${USERNAME}${END}"
+  passwd $USERNAME && clear
+  echo -e "\n${BOL_GRE}Digite a senha para ${MAG}root${END}"
+  passwd root
+}
 
 if [[ $USERNAME == mamutal91 ]]; then
+  mountStorage
   git clone https://github.com/mamutal91/dotfiles /home/mamutal91/.dotfiles
   sed -i 's/https/ssh/g' /home/mamutal91/.dotfiles/.git/config
   sed -i 's/github/git@github/g' /home/mamutal91/.dotfiles/.git/config
 fi
 
-chown -R $USERNAME:$USERNAME /home/$USERNAME
 
-clear
-cat /etc/ssh/ssh_config | grep StrictHostKeyChecking
-sleep 30
+run() {
+  createUseraAndHost
+  reflectorMirrors
+  localeAndTime
+  mkinitcpioConfigs
+  bootloaderConfigs
+  grubConfigs
+  systemdConfigs
+  sshConfigs
+  systemctlConfigs
+  sudoersConfigs
+  passwords
+}
+run "$@" || echo "$@ falhou"
--- install.sh
+++ install.sh
@@ -2,15 +2,29 @@
 
 source colors.sh
 
-read -r -p "${BOL_GRE}You username? ${END}" USERNAME
-[[ -z $USERNAME ]] && USERNAME=mamutal91 || USERNAME=$USERNAME
-echo -e "${MAG}$USERNAME${END}\n"
 clear
-read -r -p "${BOL_GRE}You hostname? ${END}" HOSTNAME
-[[ -z $HOSTNAME ]] && HOSTNAME=nitro5 || HOSTNAME=$HOSTNAME
-echo -e "${MAG}$HOSTNAME${END}\n"
 
-clear
+read -r -p "${BOL_GRE}You username? ${MAG}enter=${CYA}mamutal91${END}" USERNAME
+[[ -z $USERNAME ]] && USERNAME=mamutal91 || USERNAME=$USERNAME
+echo -e "  ${YEL}$USERNAME${END}\n"
+read -r -p "${BOL_GRE}You hostname? ${MAG}enter=${CYA}nitro5${END}" HOSTNAME
+[[ -z $HOSTNAME ]] && HOSTNAME=nitro5 || HOSTNAME=$HOSTNAME
+echo -e "  ${YEL}$HOSTNAME${END}\n"
+
+askFormatStorage() {
+  echo -n "${BOL_GRE}Você deseja formatar o ${STORAGE_HDD} (hdd)? (y/n)? ${MAG}enter=${CYA}no${END}"; read answer
+  if [[ $answer != ${answer#[Yy]} ]]; then
+    echo -n "${GRE}Você tem certeza? (y/n)? ${END}"; read answer
+    if [[ $answer != ${answer#[Yy]} ]]; then
+      formatHDD=true
+    else
+      formatHDD=false
+      echo "  ${YEL}No format ${STORAGE_HDD}${END}"
+    fi
+  else
+    echo "  ${YEL}No format ${STORAGE_HDD}${END}"
+  fi
+}
 
 if [[ $USERNAME == mamutal91 ]]; then
   SSD=/dev/nvme0n1 # ssd m2 nvme
@@ -18,21 +32,7 @@ if [[ $USERNAME == mamutal91 ]]; then
   SSD2=/dev/nvme0n1p2 # cryptswap
   SSD3=/dev/nvme0n1p3 # cryptsystem
   STORAGE_HDD=/dev/sda # hdd"
-  askFormatStorages() {
-    echo -n "Você deseja formatar o ${STORAGE_HDD} (hdd)? (y/n)? "; read answer
-    if [[ $answer != ${answer#[Yy]} ]]; then
-      echo -n "Você tem certeza? (y/n)? "; read answer
-      if [[ $answer != ${answer#[Yy]} ]]; then
-        formatHDD=true
-      else
-        formatHDD=false
-        echo No format ${STORAGE_HDD}
-      fi
-    else
-      echo No format ${STORAGE_HDD}
-    fi
-  }
-  askFormatStorages
+  askFormatStorage # Yes, i have store hdd
 else
   echo -e "Specify disks!!!
   Examples:\n\n
@@ -45,104 +45,84 @@ else
   exit 0
 fi
 
-[[ $USERNAME == mamutal91 ]] && git config --global user.email "mamutal91@gmail.com" && git config --global user.name "Alexandre Rangel"
+formatStorage() {
+  if [[ $formatHDD == true ]]; then
+  sgdisk -g --clear \
+    --new=1:0:0   --typecode=3:8300 --change-name=1:hdd \
+    $STORAGE_HDD
+  cryptsetup luksFormat --align-payload=8192 -s 256 -c aes-xts-plain64 $STORAGE_HDD
+  if [[ $? -eq 0 ]]; then
+    echo "cryptsetup luksFormat SUCCESS ${STORAGE_HDD}"
+  else
+    echo "cryptsetup luksFormat FAILURE ${STORAGE_HDD}"
+    exit 1
+  fi
+  cryptsetup luksOpen $STORAGE_HDD hdd
+  mkfs.btrfs --force --label hdd /dev/mapper/hdd
+  cryptsetup luksOpen $STORAGE_HDD hdd
+  fi
+}
 
-if [[ ${1} == recovery ]]; then
-  cryptsetup open $SSD3 system
-  cryptsetup open --type plain --key-file /dev/urandom $SSD2 swap
-  mkswap -L swap /dev/mapper/swap
-  swapon -L swap
-  args_btrfs="noatime,compress-force=zstd,commit=120,space_cache=v2,ssd"
-  mount -t btrfs -o subvol=root,$args_btrfs LABEL=system /mnt
-  mount -t btrfs -o subvol=home,$args_btrfs LABEL=system /mnt/home
-  mount -t btrfs -o subvol=snapshots,$args_btrfs LABEL=system /mnt/.snapshots
-  mount $SSD1 /mnt/boot
-  sleep 5
-  arch-chroot /mnt
-else
-  formatStorages() {
-    # Format and encrypt the hdd partition 1
-    if [[ $formatHDD == true ]]; then
-      sgdisk -g --clear \
-        --new=1:0:0       --typecode=3:8300 --change-name=1:hdd \
-        $STORAGE_HDD
-      cryptsetup luksFormat --align-payload=8192 -s 256 -c aes-xts-plain64 $STORAGE_HDD
-      if [[ $? -eq 0 ]]; then
-        echo "cryptsetup luksFormat SUCCESS ${STORAGE_HDD}"
-      else
-        echo "cryptsetup luksFormat FAILURE ${STORAGE_HDD}"
-        exit 1
-      fi
-      cryptsetup luksOpen $STORAGE_HDD hdd
-      mkfs.btrfs --force --label hdd /dev/mapper/hdd
-      cryptsetup luksOpen $STORAGE_HDD hdd
-    fi
-  }
-  formatStorages
-
-  # Format the drive
+formatDrive() {
   echo -e "\n${BOL_GRE}Formatando $SSD ${END}"
   sgdisk --zap-all $SSD
   sgdisk -g --clear \
-    --new=1:0:+1GiB   --typecode=1:ef00 --change-name=1:EFI \
-    --new=2:0:+8GiB   --typecode=2:8200 --change-name=2:cryptswap \
-    --new=3:0:0       --typecode=3:8300 --change-name=3:cryptsystem \
-    $SSD
-    if [[ $? -eq 0 ]]; then
-      echo "${GRE}sgdisk SUCCESS${END}"
-  else
-      echo "${RED}sgdisk FAILURE${END}"
-      exit 1
-  fi
+  --new=1:0:+1GiB --typecode=1:ef00 --change-name=1:EFI \
+  --new=2:0:+8GiB --typecode=2:8200 --change-name=2:cryptswap \
+  --new=3:0:0   --typecode=3:8300 --change-name=3:cryptsystem \
+  $SSD
+}
 
-  # Encrypt the system partition
+encryptSystem() {
   echo -e "\n${BOL_GRE}Criptografando partição principal - $SSD3 ${END}"
   cryptsetup luksFormat --align-payload=8192 -s 256 -c aes-xts-plain64 $SSD3
-  if [[ $? -eq 0 ]]; then
-    echo "${GRE}Crypt luksFormat SUCCESS${END}"
-  else
-    echo "${RED}Crypt luksFormat FAILURE${END}"
-    exit 1
-  fi
-  echo -e "\n${BOL_GRE}Destravando partição principal - $SSD3 ${END}"
-  cryptsetup open $SSD3 system
+}
 
-  # Enable encrypted swap partition
-  echo -e "\n${BOL_GRE}Criptografando partição swap - $SSD2 ${END}"
-  cryptsetup open --type plain --key-file /dev/urandom $SSD2 swap
+unlockDisk() {
+  echo -e "\n${BOL_GRE}Destravando partição principal - $SSD3 ${END}"
+  cryptsetup open /dev/disk/by-partlabel/cryptsystem system
+}
+
+unlockSwap() {
+  echo -e "\n${BOL_GRE}Destravando partição swap - $SSD2 ${END}"
+  cryptsetup open --type plain --key-file /dev/urandom /dev/disk/by-partlabel/cryptswap swap
+}
+
+formatSwap() {
+  echo -e "\n${BOL_GRE}Formatando e ativando partições swap - $SSD2 ${END}"
   mkswap -L swap /dev/mapper/swap
   swapon -L swap
+}
 
-  # Format the partitions
-  echo -e "\n${BOL_GRE}Formatando EFI e $SSD3 ${END}"
+formatPartitions() {
+  echo -e "\n${BOL_GRE}Formatando EFI e $SSD${END}"
   mkfs.fat -F32 -n EFI /dev/disk/by-partlabel/EFI
   mkfs.btrfs --force --label system /dev/mapper/system
+}
 
-  # Create btrfs subvolumes
+createSubVolumesBtrfs() {
   echo -e "\n${BOL_GRE}Criando volumes ${END}"
   mount -t btrfs LABEL=system /mnt
   btrfs subvolume create /mnt/root
   btrfs subvolume create /mnt/home
   btrfs subvolume create /mnt/snapshots
+}
 
-  # Mount partitions
-  echo -e "\n${BOL_GRE}Montando volumes ${END}"
-  o_btrfs="noatime,compress-force=zstd,commit=120,space_cache=v2,ssd"
-  mkdir -p /mnt/{root,home/snapshots}
+mountPartitions() {
+  echo -e "\n${BOL_GRE} volumes ${END}"
+  o="defaults,x-mount.mkdir"
+  o_btrfs="$o,noatime,compress-force=zstd,commit=120,space_cache=v2,ssd"
   umount -R /mnt
   mount -t btrfs -o subvol=root,$o_btrfs LABEL=system /mnt
   mount -t btrfs -o subvol=home,$o_btrfs LABEL=system /mnt/home
   mount -t btrfs -o subvol=snapshots,$o_btrfs LABEL=system /mnt/snapshots
-
-  echo -e "\n${BOL_GRE}Montando partição boot - $SSD1 ${END}"
+  mkdir -p /mnt/boot
   mount $SSD1 /mnt/boot
+}
 
-  sleep 200
-
-  # Discover the best mirros to download packages
+reflectorMirrors() {
   pacman -Sy reflector --noconfirm --needed
-  reflector --sort rate -l 5 --save /etc/pacman.d/mirrorlist
-  #reflector --verbose --country 'Brazil' --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+  #reflector --verbose --sort rate -l 5 --save /etc/pacman.d/mirrorlist
   sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
   sed -i 's/#UseSyslog/UseSyslog/' /etc/pacman.conf
   sed -i 's/#Color/Color\\\nILoveCandy/' /etc/pacman.conf
@@ -150,9 +130,10 @@ else
   sed -i 's/#TotalDownload/TotalDownload/' /etc/pacman.conf
   sed -i 's/#CheckSpace/CheckSpace/' /etc/pacman.conf
   sed -i "s/#VerbosePkgLists/VerbosePkgLists/g" /etc/pacman.conf
-  sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 3/g" /etc/pacman.conf
+  sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 20/g" /etc/pacman.conf
+}
 
-  # Install base and some basic tools
+pacstrapInstall() {
   pacstrap /mnt --noconfirm \
     base base-devel bash-completion \
     linux-lts linux-lts-headers linux linux-headers \
@@ -167,29 +148,30 @@ else
     imagemagick slop terminus-font noto-fonts-emoji ttf-dejavu ttf-liberation \
     xorg-server xorg-xrandr xorg-xbacklight xorg-xinit xorg-xprop xorg-server-devel xorg-xsetroot xclip xsel xautolock xorg-xdpyinfo xorg-xinput \
     atom i3-gaps i3lock alacritty thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman telegram-desktop
+}
 
-  # Generate fstab entries
+genfstabGenerator() {
   genfstab -L -p /mnt >> /mnt/etc/fstab
   sed -i "s+LABEL=swap+/dev/mapper/cryptswap+" /mnt/etc/fstab
+}
 
-  # Add cryptab entry
+cryptswapAdd() {
   echo "cryptswap $SSD2 /dev/urandom swap,offset=2048,cipher=aes-xts-plain64,size=256" >> /mnt/etc/crypttab
+}
 
-  # Copy wifi connection
+copyWifi() {
   mkdir -p /mnt/var/lib/iwd
   chmod 700 /mnt/var/lib/iwd
   cp -rf /var/lib/iwd/*.psk /mnt/var/lib/iwd
+}
 
-  # Arch Chroot
+chrootPrepare() {
   sed -i "2i USERNAME=${USERNAME}" configure.sh
   sed -i "3i HOSTNAME=${HOSTNAME}" configure.sh
   sed -i "4i SSD2=${SSD2}" configure.sh
   sed -i "5i SSD3=${SSD3}" configure.sh
   sed -i "6i STORAGE_HDD=${STORAGE_HDD}" configure.sh
-  chmod +x configure.sh
-  cp -rf configure.sh /mnt
-  clear
-  sleep 5
+  chmod +x configure.sh && cp -rf configure.sh /mnt && clear && sleep 5
   arch-chroot /mnt ./configure.sh
   if [[ $? -eq 0 ]]; then
     echo -e "\n\nFinished SUCCESS\n"
@@ -201,7 +183,41 @@ else
       arch-chroot /mnt
     fi
   else
-    echo "configure FAILURE"
+    echo "${BOL_RED}TUDO FALHOU!!!${END}"
     exit 1
   fi
+}
+
+recovery() {
+  unlockDisk
+  unlockSwap
+  formatSwap
+  mountPartitions
+  sleep 5
+  arch-chroot /mnt
+}
+
+run() {
+  formatStorage
+  formatDrive
+  encryptSystem
+  unlockDisk
+  unlockSwap
+  formatSwap
+  formatPartitions
+  createSubVolumesBtrfs
+  mountPartitions
+  reflectorMirrors
+  pacstrapInstall
+  genfstabGenerator
+  cryptswapAdd
+  copyWifi
+  chrootPrepare
+}
+
+if [[ ${1} == "recovery" ]]; then
+  recovery
+else
+  echo -e "\n${BOL_BLU}Iniciando instalação do ArchLinux${END}"
+  run "$@" || echo "$@ falhou" && exit
 fi
