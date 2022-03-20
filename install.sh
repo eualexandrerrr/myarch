a41aea01e5941f694ecbc0340fb1dfceb73bed4c
Alexandre Rangel <mamutal91@gmail.com>
2022-03-20 11:58:12 -0300

    breaking the head
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 source colors.sh
-read -r -p "${GRE}You username? ${END}" USERNAME
-[[ -z $USERNAME ]] && export USERNAME=mamutal91 || export USERNAME=$USERNAME
-echo -e "$USERNAME\n"
-read -r -p "You hostname? " HOSTNAME
-[[ -z $HOSTNAME ]] && export HOSTNAME=nitro5 || export HOSTNAME=$HOSTNAME
-echo -e "$HOSTNAME\n"
 
-export DISK_NAME="crypt"
+read -r -p "${BOL_GRE}You username? ${END}" USERNAME
+[[ -z $USERNAME ]] && USERNAME=mamutal91 || USERNAME=$USERNAME
+echo -e "${MAG}$USERNAME${END}\n"
+clear
+read -r -p "${BOL_GRE}You hostname? ${END}" HOSTNAME
+[[ -z $HOSTNAME ]] && HOSTNAME=nitro5 || HOSTNAME=$HOSTNAME
+echo -e "${MAG}$HOSTNAME${END}\n"
 
 clear
 
@@ -18,7 +18,7 @@ if [[ $USERNAME == mamutal91 ]]; then
   SSD2=/dev/nvme0n1p2 # cryptswap
   SSD3=/dev/nvme0n1p3 # cryptsystem
   STORAGE_HDD=/dev/sda # hdd"
-  askFormatStorage() {
+  askFormatStorages() {
     echo -n "Você deseja formatar o ${STORAGE_HDD} (hdd)? (y/n)? "; read answer
     if [[ $answer != ${answer#[Yy]} ]]; then
       echo -n "Você tem certeza? (y/n)? "; read answer
@@ -32,6 +32,7 @@ if [[ $USERNAME == mamutal91 ]]; then
       echo No format ${STORAGE_HDD}
     fi
   }
+  askFormatStorages
 else
   echo -e "Specify disks!!!
   Examples:\n\n
@@ -44,90 +45,104 @@ else
   exit 0
 fi
 
-formatStorage() {
-  # Format and encrypt the hdd partition 1
-  if [[ $formatHDD == true ]]; then
-    sgdisk -g --clear \
-      --new=1:0:0       --typecode=3:8300 --change-name=1:hdd \
-      $STORAGE_HDD
-    cryptsetup luksFormat --align-payload=8192 -s 256 -c aes-xts-plain64 $STORAGE_HDD
-    if [[ $? -eq 0 ]]; then
-      echo "cryptsetup luksFormat SUCCESS ${STORAGE_HDD}"
-    else
-      echo "cryptsetup luksFormat FAILURE ${STORAGE_HDD}"
-      exit 1
-    fi
-    cryptsetup luksOpen $STORAGE_HDD hdd
-    mkfs.btrfs --force --label hdd /dev/mapper/hdd
-    cryptsetup luksOpen $STORAGE_HDD hdd
-  fi
-}
-formatStorage
+[[ $USERNAME == mamutal91 ]] && git config --global user.email "mamutal91@gmail.com" && git config --global user.name "Alexandre Rangel"
 
-format() {
-  # Format storage?
-  askFormatStorage
+if [[ ${1} == recovery ]]; then
+  cryptsetup open $SSD3 system
+  cryptsetup open --type plain --key-file /dev/urandom $SSD2 swap
+  mkswap -L swap /dev/mapper/swap
+  swapon -L swap
+  args_btrfs="noatime,compress-force=zstd,commit=120,space_cache=v2,ssd"
+  mount -t btrfs -o subvol=root,$args_btrfs LABEL=system /mnt
+  mount -t btrfs -o subvol=home,$args_btrfs LABEL=system /mnt/home
+  mount -t btrfs -o subvol=snapshots,$args_btrfs LABEL=system /mnt/.snapshots
+  mount $SSD1 /mnt/boot
+  sleep 5
+  arch-chroot /mnt
+else
+  formatStorages() {
+    # Format and encrypt the hdd partition 1
+    if [[ $formatHDD == true ]]; then
+      sgdisk -g --clear \
+        --new=1:0:0       --typecode=3:8300 --change-name=1:hdd \
+        $STORAGE_HDD
+      cryptsetup luksFormat --align-payload=8192 -s 256 -c aes-xts-plain64 $STORAGE_HDD
+      if [[ $? -eq 0 ]]; then
+        echo "cryptsetup luksFormat SUCCESS ${STORAGE_HDD}"
+      else
+        echo "cryptsetup luksFormat FAILURE ${STORAGE_HDD}"
+        exit 1
+      fi
+      cryptsetup luksOpen $STORAGE_HDD hdd
+      mkfs.btrfs --force --label hdd /dev/mapper/hdd
+      cryptsetup luksOpen $STORAGE_HDD hdd
+    fi
+  }
+  formatStorages
 
   # Format the drive
+  echo -e "\n${BOL_GRE}Formatando $SSD ${END}"
   sgdisk --zap-all $SSD
   sgdisk -g --clear \
     --new=1:0:+1GiB   --typecode=1:ef00 --change-name=1:EFI \
     --new=2:0:+8GiB   --typecode=2:8200 --change-name=2:cryptswap \
     --new=3:0:0       --typecode=3:8300 --change-name=3:cryptsystem \
     $SSD
-  if [[ $? -eq 0 ]]; then
-    echo "sgdisk SUCCESS"
-    sleep 5 && clear
+    if [[ $? -eq 0 ]]; then
+      echo "${GRE}sgdisk SUCCESS${END}"
   else
-    echo "sgdisk FAILURE"
-    exit 1
+      echo "${RED}sgdisk FAILURE${END}"
+      exit 1
   fi
 
   # Encrypt the system partition
+  echo -e "\n${BOL_GRE}Criptografando partição principal - $SSD3 ${END}"
   cryptsetup luksFormat --align-payload=8192 -s 256 -c aes-xts-plain64 $SSD3
   if [[ $? -eq 0 ]]; then
-    echo "Crypt luksFormat SUCCESS"
-    sleep 5 && clear
+    echo "${GRE}Crypt luksFormat SUCCESS${END}"
   else
-    echo "Crypt luksFormat FAILURE"
+    echo "${RED}Crypt luksFormat FAILURE${END}"
     exit 1
   fi
-  cryptsetup open $SSD3 $DISK_NAME
+  echo -e "\n${BOL_GRE}Destravando partição principal - $SSD3 ${END}"
+  cryptsetup open $SSD3 system
 
   # Enable encrypted swap partition
+  echo -e "\n${BOL_GRE}Criptografando partição swap - $SSD2 ${END}"
   cryptsetup open --type plain --key-file /dev/urandom $SSD2 swap
   mkswap -L swap /dev/mapper/swap
   swapon -L swap
 
   # Format the partitions
+  echo -e "\n${BOL_GRE}Formatando EFI e $SSD3 ${END}"
   mkfs.fat -F32 -n EFI /dev/disk/by-partlabel/EFI
-  mkfs.btrfs --force --label $DISK_NAME /dev/mapper/$DISK_NAME
+  mkfs.btrfs --force --label system /dev/mapper/system
 
   # Create btrfs subvolumes
-  sleep 10 && clear
-  echo -e "Create volumes btrfs\n " #green
-  mount -t btrfs LABEL=$DISK_NAME /mnt
-  mkdir -p /mnt/{root,home,tmp,.snapshots,boot}
+  echo -e "\n${BOL_GRE}Criando volumes ${END}"
+  mount -t btrfs LABEL=system /mnt
   btrfs subvolume create /mnt/root
   btrfs subvolume create /mnt/home
-  btrfs subvolume create /mnt/tmp
-  btrfs subvolume create /mnt/.snapshots
+  btrfs subvolume create /mnt/snapshots
 
   # Mount partitions
-  echo -e "\nMount volumes btrfs\n" #green
+  echo -e "\n${BOL_GRE}Montando volumes ${END}"
+  o_btrfs="noatime,compress-force=zstd,commit=120,space_cache=v2,ssd"
+  mkdir -p /mnt/{root,home/snapshots}
   umount -R /mnt
-  args_btrfs="noatime,compress-force=zstd,commit=120,space_cache=v2,ssd"
-  mount -t btrfs -o subvol=root,$args_btrfs LABEL=$DISK_NAME /mnt
-  mount -t btrfs -o subvol=home,$args_btrfs LABEL=$DISK_NAME /mnt/home
-  mount -t btrfs -o subvol=tmp,$args_btrfs LABEL=$DISK_NAME /mnt/tmp
-  mount -t btrfs -o subvol=snapshots,$args_btrfs LABEL=$DISK_NAME /mnt/.snapshots
+  mount -t btrfs -o subvol=root,$o_btrfs LABEL=system /mnt
+  mount -t btrfs -o subvol=home,$o_btrfs LABEL=system /mnt/home
+  mount -t btrfs -o subvol=snapshots,$o_btrfs LABEL=system /mnt/snapshots
+
+  echo -e "\n${BOL_GRE}Montando partição boot - $SSD1 ${END}"
   mount $SSD1 /mnt/boot
 
-  sleep 20
+  sleep 200
 
   # Discover the best mirros to download packages
   pacman -Sy reflector --noconfirm --needed
-  reflector --verbose -c BR --protocol https --protocol http --sort rate --save /etc/pacman.d/mirrorlist
+  reflector --sort rate -l 5 --save /etc/pacman.d/mirrorlist
+  #reflector --verbose --country 'Brazil' --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
   sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
   sed -i 's/#UseSyslog/UseSyslog/' /etc/pacman.conf
   sed -i 's/#Color/Color\\\nILoveCandy/' /etc/pacman.conf
@@ -171,7 +186,6 @@ format() {
   sed -i "4i SSD2=${SSD2}" configure.sh
   sed -i "5i SSD3=${SSD3}" configure.sh
   sed -i "6i STORAGE_HDD=${STORAGE_HDD}" configure.sh
-  sed -i "7i DISK_NAME=${DISK_NAME}" configure.sh
   chmod +x configure.sh
   cp -rf configure.sh /mnt
   clear
@@ -190,24 +204,4 @@ format() {
     echo "configure FAILURE"
     exit 1
   fi
-}
-
-recovery() {
-  cryptsetup open $SSD3 $DISK_NAME
-  cryptsetup open --type plain --key-file /dev/urandom $SSD2 swap
-  mkswap -L swap /dev/mapper/swap
-  swapon -L swap
-  args_btrfs="noatime,compress-force=zstd,commit=120,space_cache=v2,ssd"
-  mount -t btrfs -o subvol=root,$args_btrfs LABEL=$DISK_NAME /mnt
-  mount -t btrfs -o subvol=home,$args_btrfs LABEL=$DISK_NAME /mnt/home
-  mount -t btrfs -o subvol=snapshots,$args_btrfs LABEL=$DISK_NAME /mnt/.snapshots
-  mount $SSD1 /mnt/boot
-  sleep 5
-  arch-chroot /mnt
-}
-
-if [[ ${1} == "recovery" ]]; then
-  recovery
-else
-  format
 fi
